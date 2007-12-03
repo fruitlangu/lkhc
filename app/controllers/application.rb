f17f7a661e24fbc1b54f8c4b4615674d36171ac3
luke.burton <luke.burton@3a0da8b3-f03f-0410-9638-b371013b665d>
2007-12-03 06:13:16 +0000

    many changes

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,4 +4,19 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_lkhc_session_id'
+  
+  def build_datetime_from_params(field_name, params)
+    DateTime.new(params["#{field_name.to_s}(1i)"].to_i, 
+             params["#{field_name.to_s}(2i)"].to_i, 
+             params["#{field_name.to_s}(3i)"].to_i,
+             params["#{field_name.to_s}(4i)"].to_i,
+             params["#{field_name.to_s}(5i)"].to_i)
+  end
+  
+  def build_date_from_params(field_name, params)
+    DateTime.new(params["#{field_name.to_s}(1i)"].to_i, 
+             params["#{field_name.to_s}(2i)"].to_i, 
+             params["#{field_name.to_s}(3i)"].to_i)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 9) do
+ActiveRecord::Schema.define(:version => 10) do
 
   create_table "categories", :force => true do |t|
     t.column "name", :string
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(:version => 9) do
     t.column "blurb",              :text
     t.column "coordinator",        :string
     t.column "staging_area_url",   :string
+    t.column "week_number",        :integer
   end
 
   create_table "series_volunteers", :force => true do |t|
